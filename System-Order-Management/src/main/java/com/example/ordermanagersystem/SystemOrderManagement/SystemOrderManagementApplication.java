2d1d15ee682e669c555fb20ae73b0cf42dd54a12
Kateryna2882 <govoryhina82@gmail.com>
2024-01-21 17:18:29 +0200

    Initial commit

---
+++ System-Order-Management/src/main/java/com/example/ordermanagersystem/SystemOrderManagement/SystemOrderManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.ordermanagersystem.SystemOrderManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemOrderManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SystemOrderManagementApplication.class, args);
+	}
+
+}
---
+++ System-Order-Management/src/test/java/com/example/ordermanagersystem/SystemOrderManagement/SystemOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordermanagersystem.SystemOrderManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
